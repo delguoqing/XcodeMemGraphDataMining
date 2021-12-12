6a440be71436c67173d297ee59b6b66008acc265
Qing Guo <delguoqing@hotmail.com>
2021-12-12 23:32:42 +0800

    validating all lines

--- unity.py
+++ unity.py
@@ -2,12 +2,187 @@
 import sys
 import subprocess
 import os
+import re
 
 HEAP = "heap"
 MALLOC_HISTORY = "malloc_history"
 
 START_MARK = "Call graph:"
 END_MARK = "Total number in stack -- this line is here to get the correct format for importing with the Sampler instrument in Instruments.app"
+
+# 这个工具打印的size，最多只会有3个有效数字
+# 所以KB=1000
+KB = 1024
+MB = KB * KB
+GB = KB * KB * KB
+
+def toSizeFloat(number, div):
+    a = number // div
+    b = number % div
+    ret = str(a)
+    rem = 3 - len(ret)
+    if rem == 0:
+        return ret
+    strB = str(b)
+    strB = strB.zfill(len(str(div)) - 1)
+    ret += "." + strB[:rem]
+    zeroPadding = rem - len(strB)
+    if zeroPadding > 0:
+        return ret + "0" * zeroPadding
+    else:
+        return ret
+
+# 这两个函数的写法是为了保证提取信息和转换回去的结果能完全一致
+# 这样才能够用sample数据来做测试
+# 可能有更简单的写法，太久不写python忘了
+def sizeToStr(size):
+    if size < KB:
+        return "%d bytes" % size
+    elif size < MB:
+        return "%sK" % (toSizeFloat(size, KB))
+    elif size < GB:
+        return "%sM" % (toSizeFloat(size, MB))
+    else:
+        return "%sG" % (toSizeFloat(size, GB))
+
+def strToSize(sizeStr):
+    digitEnd = len(sizeStr)
+    dotPosition = -1
+    for i in range(len(sizeStr)):
+        if sizeStr[i] == ".":
+            dotPosition = i
+        elif not sizeStr[i].isdigit():
+            digitEnd = i
+            break
+    
+    if dotPosition == -1:
+        a = int(sizeStr[:digitEnd])
+        b = 0
+        div = 1
+    else:
+        a = int(sizeStr[:dotPosition])
+        b = int(sizeStr[dotPosition + 1: digitEnd])
+        div = 10 ** (digitEnd - dotPosition - 1)
+
+    unit = sizeStr[digitEnd:]
+    if unit == "G":
+        mul = GB
+    elif unit == "M":
+        mul = MB
+    elif unit == "K":
+        mul = KB
+    else:
+        mul = 1
+
+    a *= mul
+    b *= mul
+    return a + b // div
+
+class Node(object):
+    """
+    name can be "???", in that case, a load address will be used instead.
+    """
+    def __init__(self):
+        self.count = 0
+        self.size = 0
+        self.name = ""
+        self.moduleName = ""
+        self.address = 0
+        self.offset = 0
+        self.loadAddress = 0
+
+    def getReadableSize(self):
+        return sizeToStr(self.size)
+
+    def __str__(self):
+        ret = str(self.count)
+        if self.size != 0:
+            ret += " (%s)" % self.getReadableSize()
+        ret += " %s  (in %s)" % (self.name, self.moduleName)
+        if self.name == "???":
+            ret += "  load address 0x%x" % self.loadAddress
+        if self.offset < 56392:    # do not know exactly what is this number
+            ret += " + %d  [0x%x]" % (self.offset, self.address)
+        else:
+            ret += " + 0x%x  [0x%x]" % (self.offset, self.address)
+        return ret
+
+def findStartOfLine(lineStr):
+    for i in range(len(lineStr)):
+        if lineStr[i] not in " +!:|":
+            return i, i
+    raise RuntimeError("invalid line!")
+
+def findCount(lineStr, start):
+    end = len(lineStr)
+    for j in range(start, len(lineStr)):
+        if not lineStr[j].isdigit():
+            end = j
+            break
+    if end == start:
+        raise RuntimeError("invalid line! can't parse count")
+    return int(lineStr[start:end]), end
+
+def findSize(lineStr, start):
+    oldStart = start
+    start = lineStr.find('(', start)
+    if start == -1:
+        raise RuntimeError("invalid line! can't parse size!")
+    if lineStr[start:start+4] == "(in ":    # 我们可能没有size，不知道为啥。。。
+        return 0, oldStart
+    end = lineStr.find(')', start)
+    if end == -1 or end == start + 1:
+        raise RuntimeError("invalid line! can't parse size!")
+    return strToSize(lineStr[start + 1: end]), end + 1
+
+def findName(lineStr, start):
+    end = lineStr.find("(in ", start)
+    if end == -1:
+        raise RuntimeError("invalid line! can't parse name!")
+    return lineStr[start:end].strip(), end
+
+def findModuleName(lineStr, start):
+    start = lineStr.find("(in ", start) + 4
+    end = lineStr.find(")", start + 1)
+    return lineStr[start: end], end + 1
+
+def findLoadAddress(lineStr, start):
+    mark = "load address "
+    start = lineStr.find(mark, start) + len(mark)
+    end = lineStr.find("+", start)
+    return int(lineStr[start: end].strip(), 16), end
+
+def findOffset(lineStr, start):
+    start = lineStr.find("+", start) + 1
+    end = lineStr.find("[", start)
+    token = lineStr[start: end].strip()
+    if token.startswith("0x"):
+        return int(token, 16), end
+    else:
+        return int(token), end
+
+def findAddress(lineStr, start):
+    start = lineStr.find("[", start) + 1
+    end = lineStr.find("]", start + 1)
+    return int(lineStr[start: end], 16), end + 1
+
+def buildNode(lineStr):
+    print (lineStr)
+    nd = Node()
+    indent, offset = findStartOfLine(lineStr)
+    nd.count, offset = findCount(lineStr, offset)
+    nd.size, offset = findSize(lineStr, offset)
+    nd.name, offset = findName(lineStr, offset)
+    nd.moduleName, offset = findModuleName(lineStr, offset)
+    if nd.name == "???":
+        nd.loadAddress, offset = findLoadAddress(lineStr, offset)
+    nd.offset, offset = findOffset(lineStr, offset)
+    nd.address, offset = findAddress(lineStr, offset)
+    if str(nd) != lineStr[indent:]:
+        print(str(nd))
+        print(lineStr[indent:])
+        assert False
+    return nd
 
 def buildTree(treeStr):
     start = treeStr.find(START_MARK)
@@ -18,6 +193,9 @@ def buildTree(treeStr):
     if end == -1:
         return False
     lines = treeStr[start:end].strip().split(os.linesep)
+    # tests
+    for line in lines[1:]:
+        buildNode(line)
     return True
 
 if __name__ == "__main__":
